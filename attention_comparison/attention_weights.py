243ade6b7959d8d8c9e076771ad26a18d4df45b1
Alessandro Pilotti <apilotti@cloudbasesolutions.com>
2024-10-31 08:37:27 +0100

    Use pd.concat in get_attention_weights to improve memory usage

--- attention_comparison/attention_weights.py
+++ attention_comparison/attention_weights.py
@@ -52,7 +52,7 @@ def get_attention_weights(model_name, model_path, use_default_model_tokenizer,
     LOG.info(f"Using device: {device}")
     model = model.to(device)
 
-    attentions = []
+    attentions = None
 
     for sequence_index, sequence in enumerate(sequences, 1):
         LOG.info(f"Sequence {sequence_index} out of {len(sequences)}")
@@ -142,9 +142,12 @@ def get_attention_weights(model_name, model_path, use_default_model_tokenizer,
                      'Seq_1', 'Seq_2'],
                     inplace=True)
 
-                attentions.append(df)
+                if attentions is None:
+                    attentions = df
+                else:
+                    attentions = pd.concat([attentions, df])
 
-    return pd.concat(attentions)
+    return attentions
 
 
 def save_attentions(attentions, output_path):
